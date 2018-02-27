29c753acd67bc1908baa6a334e3e5c70599d993e
Pavel Demin <pavel-demin@outlook.com>
2018-02-27 13:30:50 +0100

    add axis_detector_reader and muoscope/block_design.tcl

---
+++ cores/axis_detector_reader_v1_0/axis_detector_reader.v
@@ -0,0 +1,115 @@
+
+`timescale 1 ns / 1 ps
+
+module axis_detector_reader
+(
+  // System signals
+  input  wire         aclk,
+  input  wire         aresetn,
+
+  input  wire [63:0]  det_data,
+  input  wire [2:0]   cfg_data,
+
+  // Master side
+  input  wire         m_axis_tready,
+  output wire [127:0] m_axis_tdata,
+  output wire         m_axis_tvalid
+);
+
+  reg [63:0] int_data_reg[2:0], int_data_next[2:0];
+  reg [63:0] int_time_reg, int_time_next;
+  reg [5:0] int_cntr_reg, int_cntr_next;
+  reg [3:0] int_or_reg, int_or_next;
+  reg [2:0] int_sum_reg, int_sum_next;
+  reg [2:0] int_case_reg, int_case_next;
+  reg int_tvalid_reg, int_tvalid_next;
+
+  always @(posedge aclk)
+  begin
+    if(~aresetn)
+    begin
+      int_data_reg[0] <= 64'd0;
+      int_data_reg[1] <= 64'd0;
+      int_data_reg[2] <= 64'd0;
+      int_time_reg <= 64'd0;
+      int_cntr_reg <= 6'd0;
+      int_or_reg <= 4'd0;
+      int_sum_reg <= 4'd0;
+      int_case_reg <= 3'd0;
+      int_tvalid_reg <= 1'b0;
+    end
+    else
+    begin
+      int_data_reg[0] <= int_data_next[0];
+      int_data_reg[1] <= int_data_next[1];
+      int_data_reg[2] <= int_data_next[2];
+      int_time_reg <= int_time_next;
+      int_cntr_reg <= int_cntr_next;
+      int_or_reg <= int_or_next;
+      int_sum_reg <= int_sum_next;
+      int_case_reg <= int_case_next;
+      int_tvalid_reg <= int_tvalid_next;
+    end
+  end
+
+  always @*
+  begin
+    int_data_next[0] = det_data;
+    int_data_next[1] = int_data_reg[0];
+    int_data_next[2] = int_data_reg[2];
+    int_time_next = int_time_reg + 1'b1;
+    int_cntr_next = int_cntr_reg;
+    int_or_next = int_or_reg;
+    int_sum_next = int_sum_reg;
+    int_case_next = int_case_reg;
+    int_tvalid_next = int_tvalid_reg;
+
+    case(int_case_reg)
+      0:
+      begin
+        if(|int_data_reg[1])
+        begin
+          int_case_next = 3'd1;
+          int_cntr_next = 6'd0;
+          int_data_next[2] = int_data_reg[1];
+        end
+      end
+      1:
+      begin
+        int_cntr_next = int_cntr_reg + 1'b1;
+        int_data_next[2] = int_data_reg[2] | int_data_reg[1];
+        if(&int_cntr_reg)
+        begin
+          int_case_next = 3'd2;
+        end
+      end
+      2:
+      begin
+        int_or_next = {|int_data_reg[2][63:48], |int_data_reg[2][47:32], |int_data_reg[2][31:16], |int_data_reg[2][15:0]};
+        int_case_next = 3'd3;
+      end
+      3:
+      begin
+        int_sum_next = int_or_reg[3] + int_or_reg[2] + int_or_reg[1] + int_or_reg[0];
+        int_case_next = 3'd4;
+      end
+      4:
+      begin
+        int_tvalid_next = int_sum_reg >= cfg_data;
+        int_case_next = 3'd5;
+      end
+      5:
+      begin
+        if(m_axis_tready)
+        begin
+          int_tvalid_next = 1'b0;
+          int_case_next = 3'd0;
+        end
+      end
+    endcase
+  end
+
+  assign m_axis_tdata = {int_time_reg, int_data_reg[2]};
+  assign m_axis_tvalid = int_tvalid_reg;
+
+endmodule
---
+++ projects/muoscope/block_design.tcl
@@ -0,0 +1,184 @@
+# Create processing_system7
+cell xilinx.com:ip:processing_system7:5.5 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/te0720-1cf.xml
+} {
+  M_AXI_GP0_ACLK ps_0/FCLK_CLK0
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant:1.1 const_0
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset:5.0 rst_0 {} {
+  ext_reset_in const_0/dout
+}
+
+# System controller support logic
+
+connect_bd_net [get_bd_ports PL_pin_P22] [get_bd_pins ps_0/I2C1_SDA_I]
+
+# Create util_vector_logic
+cell xilinx.com:ip:util_vector_logic:2.0 or_0 {
+  C_SIZE 1
+  C_OPERATION or
+} {
+  Op1 ps_0/I2C1_SDA_O
+  Op2 ps_0/I2C1_SDA_T
+  Res PL_pin_N22
+}
+
+# Create util_vector_logic
+cell xilinx.com:ip:util_vector_logic:2.0 or_1 {
+  C_SIZE 1
+  C_OPERATION or
+} {
+  Op1 ps_0/I2C1_SCL_O
+  Op2 ps_0/I2C1_SCL_T
+  Res ps_0/I2C1_SCL_I
+  Res PL_pin_L16
+}
+
+# LED
+
+# Create c_counter_binary
+cell xilinx.com:ip:c_counter_binary:12.0 cntr_0 {
+  Output_Width 32
+} {
+  CLK ps_0/FCLK_CLK0
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_0 {
+  DIN_WIDTH 32 DIN_FROM 26 DIN_TO 26 DOUT_WIDTH 1
+} {
+  Din cntr_0/Q
+  Dout led_o
+}
+
+# CFG
+
+# Create axi_cfg_register
+cell pavel-demin:user:axi_cfg_register:1.0 cfg_0 {
+  CFG_DATA_WIDTH 64
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_1 {
+  DIN_WIDTH 64 DIN_FROM 0 DIN_TO 0 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_2 {
+  DIN_WIDTH 64 DIN_FROM 1 DIN_TO 1 DOUT_WIDTH 1
+} {
+  Din cfg_0/cfg_data
+}
+
+# Create xlslice
+cell xilinx.com:ip:xlslice:1.0 slice_3 {
+  DIN_WIDTH 64 DIN_FROM 34 DIN_TO 32 DOUT_WIDTH 3
+} {
+  Din cfg_0/cfg_data
+}
+
+# DATA
+
+# Create util_ds_buf
+cell xilinx.com:ip:util_ds_buf:2.1 buf_0 {
+  C_SIZE 64
+  C_BUF_TYPE IBUFDS
+} {
+  IBUF_DS_P data_p_i
+  IBUF_DS_N data_n_i
+}
+
+# Create axis_detector_reader
+cell pavel-demin:user:axis_detector_reader:1.0 det_0 {} {
+  det_data buf_0/IBUF_OUT
+  cfg_data slice_3/Dout
+  aclk ps_0/FCLK_CLK0
+  aresetn slice_1/Dout
+}
+
+# Create fifo_generator
+cell xilinx.com:ip:fifo_generator:13.1 fifo_generator_0 {
+  PERFORMANCE_OPTIONS First_Word_Fall_Through
+  INPUT_DATA_WIDTH 128
+  INPUT_DEPTH 512
+  OUTPUT_DATA_WIDTH 32
+  OUTPUT_DEPTH 2048
+  READ_DATA_COUNT true
+  READ_DATA_COUNT_WIDTH 12
+} {
+  clk ps_0/FCLK_CLK0
+  srst slice_2/Dout
+}
+
+# Create axis_fifo
+cell pavel-demin:user:axis_fifo:1.0 fifo_0 {
+  S_AXIS_TDATA_WIDTH 128
+  M_AXIS_TDATA_WIDTH 32
+} {
+  S_AXIS det_0/M_AXIS
+  FIFO_READ fifo_generator_0/FIFO_READ
+  FIFO_WRITE fifo_generator_0/FIFO_WRITE
+  aclk ps_0/FCLK_CLK0
+}
+
+# Create axi_axis_reader
+cell pavel-demin:user:axi_axis_reader:1.0 reader_0 {
+  AXI_DATA_WIDTH 32
+} {
+  S_AXIS fifo_0/M_AXIS
+  aclk ps_0/FCLK_CLK0
+  aresetn rst_0/peripheral_aresetn
+}
+
+# STS
+
+# Create axi_sts_register
+cell pavel-demin:user:axi_sts_register:1.0 sts_0 {
+  STS_DATA_WIDTH 32
+  AXI_ADDR_WIDTH 32
+  AXI_DATA_WIDTH 32
+} {
+  sts_data fifo_generator_0/rd_data_count
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins sts_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
+set_property OFFSET 0x40000000 [get_bd_addr_segs ps_0/Data/SEG_sts_0_reg0]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins cfg_0/S_AXI]
+
+set_property RANGE 4K [get_bd_addr_segs ps_0/Data/SEG_cfg_0_reg0]
+set_property OFFSET 0x40001000 [get_bd_addr_segs ps_0/Data/SEG_cfg_0_reg0]
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:axi4 -config {
+  Master /ps_0/M_AXI_GP0
+  Clk Auto
+} [get_bd_intf_pins reader_0/S_AXI]
+
+set_property RANGE 8K [get_bd_addr_segs ps_0/Data/SEG_reader_0_reg0]
+set_property OFFSET 0x40002000 [get_bd_addr_segs ps_0/Data/SEG_reader_0_reg0]
