2c22d36f2c6491b5e7b9bafd33535c024ee84c95
Pavel Demin <pavel-demin@outlook.com>
2022-10-05 11:35:54 +0200

    simplify axis_detector_reader

--- cores/axis_detector_reader_v1_0/axis_detector_reader.v
+++ cores/axis_detector_reader_v1_0/axis_detector_reader.v
@@ -15,8 +15,8 @@ module axis_detector_reader
   output wire         m_axis_tvalid
 );
 
-  reg [63:0] int_data_reg, int_data_next;
   reg [63:0] int_time_reg, int_time_next;
+  reg [63:0] int_data_reg, int_data_next;
   reg [7:0] int_cntr_reg, int_cntr_next;
   reg [3:0] int_or_reg, int_or_next;
   reg [2:0] int_sum_reg, int_sum_next;
@@ -27,41 +27,42 @@ module axis_detector_reader
   begin
     if(~aresetn)
     begin
-      int_data_reg <= 64'd0;
       int_time_reg <= 64'd0;
+      int_data_reg <= 64'd0;
       int_cntr_reg <= 8'd0;
       int_or_reg <= 4'd0;
       int_sum_reg <= 4'd0;
-      int_case_reg <= 3'd0;
       int_tvalid_reg <= 1'b0;
+      int_case_reg <= 3'd0;
     end
     else
     begin
-      int_data_reg <= int_data_next;
       int_time_reg <= int_time_next;
+      int_data_reg <= int_data_next;
       int_cntr_reg <= int_cntr_next;
       int_or_reg <= int_or_next;
       int_sum_reg <= int_sum_next;
-      int_case_reg <= int_case_next;
       int_tvalid_reg <= int_tvalid_next;
+      int_case_reg <= int_case_next;
     end
   end
 
   always @*
   begin
-    int_data_next = int_data_reg;
     int_time_next = int_time_reg + 1'b1;
+    int_data_next = int_data_reg;
     int_cntr_next = int_cntr_reg;
     int_or_next = int_or_reg;
     int_sum_next = int_sum_reg;
-    int_case_next = int_case_reg;
     int_tvalid_next = int_tvalid_reg;
+    int_case_next = int_case_reg;
 
     case(int_case_reg)
       0:
       begin
-        int_cntr_next = 8'd0;
         int_data_next = din;
+        int_cntr_next = 8'd0;
+        int_tvalid_next = 1'b0;
         if(|din)
         begin
           int_case_next = 3'd1;
@@ -69,8 +70,8 @@ module axis_detector_reader
       end
       1:
       begin
-        int_cntr_next = int_cntr_reg + 1'b1;
         int_data_next = int_data_reg | din;
+        int_cntr_next = int_cntr_reg + 1'b1;
         if(int_cntr_reg >= cfg[7:0])
         begin
           int_case_next = 3'd2;
@@ -88,19 +89,7 @@ module axis_detector_reader
       end
       4:
       begin
-        if(int_sum_reg >= cfg[10:8])
-        begin
-          int_tvalid_next = 1'b1;
-          int_case_next = 3'd5;
-        end
-        else
-        begin
-          int_case_next = 3'd0;
-        end
-      end
-      5:
-      begin
-        int_tvalid_next = 1'b0;
+        int_tvalid_next = int_sum_reg >= cfg[10:8];
         int_case_next = 3'd0;
       end
     endcase
