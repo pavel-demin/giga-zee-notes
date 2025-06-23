50be64edc3393485ea98d1781fde306288a9839f
Pavel Demin <pavel-demin@outlook.com>
2025-06-23 16:34:10 +0200

    simplify axis_window

--- cores/axis_window.v
+++ cores/axis_window.v
@@ -18,9 +18,11 @@ module axis_window
   output wire         m_axis_tvalid
 );
 
-  reg [127:0] int_tdata_reg, int_tdata_next;
-  reg [7:0] int_cntr_reg, int_cntr_next;
-  reg int_tvalid_reg, int_tvalid_next;
+  reg [127:0] int_tdata_reg;
+  reg [7:0] int_cntr_reg = 8'd0;
+  reg int_tvalid_reg = 1'b0;
+
+  wire int_enbl_wire = |int_cntr_reg;
 
   always @(posedge aclk)
   begin
@@ -32,30 +34,19 @@ module axis_window
     end
     else
     begin
-      int_tdata_reg <= int_tdata_next;
-      int_cntr_reg <= int_cntr_next;
-      int_tvalid_reg <= int_tvalid_next;
+      int_tdata_reg <= s_axis_tdata;
+
+      if(int_enbl_wire)
+      begin
+        int_cntr_reg <= int_cntr_reg - 1'b1;
+      end
+      else if(s_axis_tvalid)
+      begin
+        int_cntr_reg <= cfg;
+      end
+
+      int_tvalid_reg <= int_enbl_wire | s_axis_tvalid;
     end
-  end
-
-  always @*
-  begin
-    int_tdata_next = int_tdata_reg;
-    int_cntr_next = int_cntr_reg;
-    int_tvalid_next = int_tvalid_reg;
-
-    if(|int_cntr_reg)
-    begin
-      int_cntr_next = int_cntr_reg - 1'b1;
-    end
-
-    if(s_axis_tvalid & ~|int_cntr_reg)
-    begin
-      int_cntr_next = cfg;
-    end
-
-    int_tdata_next = s_axis_tdata;
-    int_tvalid_next = |int_cntr_reg | s_axis_tvalid;
   end
 
   assign m_axis_tdata = int_tdata_reg;
